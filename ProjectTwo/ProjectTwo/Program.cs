674b882c36e44eed88752c79945d1ce72c3570d6
Itz Firoz <mdfirozahemd698@gmail.com>
2024-01-01 18:51:19 +0600

    Factiroal Number Complate

--- ProjectTwo/ProjectTwo/Program.cs
+++ ProjectTwo/ProjectTwo/Program.cs
@@ -1,2 +1,13 @@
 ﻿//Factorial of a Number in C#
-Console.WriteLine("Hello");
+
+
+
+Console.Write("Factorial Numebr : ");
+int data =Convert.ToInt32( Console.ReadLine());
+int Store = 1;
+for (int i = 0; i < data; i++)
+{
+    
+     Store += Store * i;
+}
+Console.WriteLine(Store);
---
+++ ProjectTwo/Reverse of a Number/Program.cs
@@ -0,0 +1,2 @@
+﻿//Reverse of a Number
+
