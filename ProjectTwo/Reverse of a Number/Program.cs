a989b81dd04eea2d70cfbef0ffe6273d490f4054
Itz Firoz <mdfirozahemd698@gmail.com>
2024-01-01 21:21:01 +0600

    Revarse Number array

--- ProjectTwo/Reverse of a Number/Program.cs
+++ ProjectTwo/Reverse of a Number/Program.cs
@@ -1,2 +1,15 @@
 ﻿//Reverse of a Number
 
+int[] arrayStort = { 1,2,3,4,5};
+int[] Stroee = new int[arrayStort.Length];
+
+for (int i = arrayStort.Length -1; i >=0; i--)
+{
+    Stroee[i] = arrayStort[i];
+    Console.Write(arrayStort[i]);
+}
+Console.WriteLine(" ");
+foreach (int i in Stroee)
+{
+    Console.WriteLine(i);
+}
